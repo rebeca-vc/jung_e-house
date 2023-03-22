9e773a639b8b797ff1fdf3db5d4152f85de99892
Rebeca <rebeca.vcarvalho78@gmail.com>
2023-03-22 11:34:12 -0300

    Interact With Components Using Events chapter

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import Banner from "./Banner";
 import Cart from "./Cart";
 import ShoppingList from "./ShoppingList";
+// import QuestionForm from "./QuestionForm";
+import Footer from "./Footer";
 
 const App = () => {
   return (<div>
   <Banner />
   <ShoppingList />
   <Cart />
+  {/* <QuestionForm /> */}
+  <Footer />
   </div>);
 }
 
